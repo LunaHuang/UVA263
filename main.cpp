628b5cab1c452b35149c280dcc42d1492951acca
Luna Huang <lunahuang@weintek.com>
2022-03-05 10:50:59 +0800

    Create code to support UVA263

---
+++ main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <exception>
+#include <math.h>
+
+#include <gtest/gtest.h>
+
+std::vector<int> NumberSplit(int number)
+{
+	std::vector<int> vSplit;
+	for(; number; number/=10)
+		vSplit.push_back( number % 10 );
+    return vSplit;
+}
+
+int HBubbleSort(std::vector<int> array)
+{
+	int size = array.size();
+    int number = 0;
+    for (int i = size - 1; i > 0; --i) {
+        for (int j = 0; j < i; ++j)
+            if (array[j] > array[j + 1])
+                std::swap(array[j], array[j + 1]);
+    }
+
+    for(int i = 0; i < size; i++){
+//		std::cout << "number " << i << " " << array[i] << " ";
+		number = number + array[i] * pow(10, i);
+//		std::cout << "\n number : " << number <<std::endl;
+    }
+    
+    return number;
+}
+
+int LBubbleSort(std::vector<int> array)
+{
+	int size = array.size();
+    int number = 0;
+    for (int i = size - 1; i > 0; --i) {
+        for (int j = 0; j < i; ++j)
+            if (array[j] < array[j + 1])
+                std::swap(array[j], array[j + 1]);
+    }
+
+    for(int i = size - 1; i >= 0; --i){
+//		std::cout << "number " << i << " " << array[i] << " ";
+		number = number + array[i] * pow(10, i);
+//		std::cout << "\n number : " << number <<std::endl;
+    }
+    
+    return number;
+}
+
+bool CheckNumber(std::string str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (std::isdigit(str[i]) == false)
+			return false;
+    }
+	return true;
+}
+
+static int chain = 0;
+static std::vector<int> leaves;
+static void TestFunction(int max, int min)
+{
+    int leave, size;
+    int b_val = max;
+    int l_val = min;
+
+    leave = b_val - l_val;
+    chain ++;
+	std::cout << b_val << " - " << l_val << " = " << leave << std::endl;
+    size = leaves.size();
+/*
+    std::cout << "leaves size: " << size << std::endl;
+    for(int i=0; i< size; i++)
+		std::cout << " " << leaves[i] << " ";
+	std::cout << std::endl;
+*/
+    if(size != 0){
+        for(int i=0; i< size; i++){
+			if(leaves[i] == leave)
+				return;
+        }
+	}
+    leaves.push_back(leave);
+    std::vector<int> r_v = NumberSplit(leave);
+    TestFunction(HBubbleSort(r_v), LBubbleSort(r_v));
+
+}
+
+TEST(testCase, test0){
+    int number = 45321;
+    std::vector<int> v = NumberSplit(number);
+    for(int i; i < v.size(); i++){
+       int mod = number % (i * 10);
+       EXPECT_EQ(v[i],mod);
+       std::string s = std::to_string(mod);
+       EXPECT_STREQ("mod : %d", s.c_str());
+    }
+}
+
+TEST(testCase, test1){
+	static const int arr[] = {3,2,4,5,6,3};
+	std::vector<int> v(arr, arr + sizeof(arr) / sizeof(arr[0]));
+    EXPECT_EQ(LBubbleSort(v),233456);
+}
+
+TEST(testCase, test2){
+	static const int arr[] = {3,2,4,5,6,3};
+	std::vector<int> v(arr, arr + sizeof(arr) / sizeof(arr[0]));
+    EXPECT_EQ(HBubbleSort(v),654332);
+//    EXPECT_EQ(HBubbleSort(v),564332); // fail test
+}
+
+int main(int argc, char **argv) {
+    int ret;
+    if( argc != 2) {
+		std::cout << "Please input one number " << std::endl;
+		return false;
+    }
+    if( CheckNumber(argv[1]) == false){
+		std::cout << "Incorrect input, please input number " << std::endl;
+		return false;
+	}
+
+    testing::InitGoogleTest(&argc, argv);
+    ret = RUN_ALL_TESTS();
+    int input_number = atoi(argv[1]);
+    std::cout << "Original number was " << input_number <<std::endl;
+    std::vector<int> r_v = NumberSplit(input_number);
+    TestFunction(HBubbleSort(r_v), LBubbleSort(r_v));
+    std::cout << "Chain length " << chain << std::endl;
+    return 0;
+}
+
